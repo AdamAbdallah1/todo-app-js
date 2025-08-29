d9b40bd62f1194c8919579de97fc6e75f72ca68e
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-08-29 16:59:57 +0300

    Adding some condition

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     addTaskBtn.addEventListener("click", () => {
         taskList = document.getElementById("task-list");
         task = document.getElementById("task-input").value;
-        list = document.createElement("li");
-        list.textContent = task;
-        taskList.appendChild(list);
+        if (task) {
+            list = document.createElement("li");
+            list.textContent = task;
+            taskList.appendChild(list);
+        }
     })
 })
