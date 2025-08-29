053b05af24d9f6de560848f00da16ed87c6453aa
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-08-29 16:58:05 +0300

    Creating js to get button and task to display it

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    addTaskBtn = document.getElementById("add-btn");
+    addTaskBtn.addEventListener("click", () => {
+        taskList = document.getElementById("task-list");
+        task = document.getElementById("task-input").value;
+        list = document.createElement("li");
+        list.textContent = task;
+        taskList.appendChild(list);
+    })
+})
