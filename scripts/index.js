c55cdda1e25d22fbd3b77cb28cab62caa3c868b8
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-08-29 17:48:45 +0300

    Wrapping buttons

--- scripts/index.js
+++ scripts/index.js
@@ -10,8 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
             list.appendChild(span);
 
             const deleteBtn = document.createElement("button");
-            deleteBtn.style.marginLeft = "10px";
-            deleteBtn.style.background = "black"; 
+            deleteBtn.style.background = "red"; 
             deleteBtn.style.border = "none";
             deleteBtn.style.cursor = "pointer";
 
@@ -27,10 +26,29 @@ document.addEventListener("DOMContentLoaded", () => {
                 list.remove();
             });
 
-            list.appendChild(deleteBtn);
             taskList.appendChild(list);
 
+            const editButton = document.createElement("button");
+            editButton.style.background = "#45a049";
+            editButton.style.border = "none";
+            editButton.style.cursor = "pointer";
+
+            const editIcon = document.createElement("img");
+            editIcon.src = "../assets/edit-icon.png";
+            editIcon.alt = "Edit";
+            editIcon.style.width = "20px";
+            editIcon.style.height = "20px";
+
+            editButton.appendChild(editIcon);
+
             document.getElementById("task-input").value = "";
+            controller = document.createElement("div");
+            controller.style.display = "flex";
+            controller.style.gap = "10px"
+
+            controller.appendChild(deleteBtn);
+            controller.appendChild(editButton);
+            list.appendChild(controller)
         }
     })
 })
